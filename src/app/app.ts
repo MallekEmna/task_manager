6dfb5f6f4346c8d1ea72719d998d64099f134b7b
MallekEmna <emna.mallek96@gmail.com>
2025-11-08 09:35:17 +0100

    edit project icons probleme solved

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectList } from './features/projects/components/project-list/project-list';
-import { LucideAngularModule } from 'lucide-angular';
 import { Navbar } from './features/projects/components/navbar/navbar';
+import { LucideIconsModule } from './lucide-icons.module';
 @Component({
   selector: 'app-root',
-  imports: [LucideAngularModule, Navbar, RouterOutlet],
+  imports: [LucideIconsModule, Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
